fe926be00d005bf7f694869b563ceef7c745edc4
Mohammedi Abdenasser <75934365+mdinacer@users.noreply.github.com>
2024-09-12 14:41:04 +0100

    added tamagui inter font to root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -29,8 +29,8 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf')
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
